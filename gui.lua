430281a774db48d60e2e578d147e994d116ac649
loglizzy <72479668+loglizzy@users.noreply.github.com>
2021-10-03 05:42:26 -0400

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -75,6 +75,45 @@ title.TextColor3 = Color3.fromRGB(189, 189, 189)
 title.TextSize = 14.000
 title.TextXAlignment = Enum.TextXAlignment.Left
 
+local UserInputService = game:GetService("UserInputService")
+local gui = Frame
+
+local dragging
+local dragInput
+local dragStart
+local startPos
+
+local function update(input)
+	local delta = input.Position - dragStart
+	gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+end
+
+gui.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+		dragging = true
+		dragStart = input.Position
+		startPos = gui.Position
+		
+		input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+			end
+		end)
+	end
+end)
+
+gui.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+		dragInput = input
+	end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+	if input == dragInput and dragging then
+		update(input)
+	end
+end)
+
 return {new=function(v,e)
     local nw = Instance.new("TextButton")
     local icon = Instance.new("ImageButton")
