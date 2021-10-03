0057a84dcb7bf5c90ac570dec3f5d68ac0160d3b
loglizzy <72479668+loglizzy@users.noreply.github.com>
2021-10-03 02:56:02 -0400

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,202 @@
+return {
+    [1001] = {
+		"http://www.roblox.com/asset/?id=105624953",
+		"Mosin-Nagant",
+		"Although old, this Mosin-Nagant rifle is effective at long range and powerful, but has a slow rate of fire. Uses a 7.62x54mm round with a five-round magazine."},
+	[1002] = {
+		"http://www.roblox.com/asset/?id=105624960",
+		"Shotgun",
+		"A 12-gauge double-barreled shotgun. This weapon is very powerful at close range, but has a slow rate of fire."},
+	[1003] = {
+		"http://www.roblox.com/asset/?id=116674232",
+		"AK-47",
+		"The AK-47 is a high-powered assault rifle effective at medium range. Uses a 7.62 x 39mm round."},
+	[1004] = {
+		"http://www.roblox.com/asset/?id=105624893",
+		"M4A1",
+		"The M4A1 is a powerful assault rifle effective at long range. Compatible with 5.56 x 45mm STANAG magazines."},
+	[1005] = {
+		"http://www.roblox.com/asset/?id=110672006",
+		"Mk 48",
+		"The Mk 48 is a lightweight machine gun capable of devastation on a large scale effective at medium range. Uses a 7.62x51mm round with a 100-round box magazine."},
+	[1006] = {
+		"http://www.roblox.com/asset/?id=105624890",
+		"M1 Garand",
+		"The M1 Garand is a semi-automatic rifle effective at long range. Uses a 7.62 x 63mm round with an eight-round 'en bloc' clip. No longer comes in pink."},
+	[1010] = {
+		"http://www.roblox.com/asset/?id=105624921",
+		"Maverick",
+		"The Maverick 88 is a 20-gauge hunting shotgun effective at short range."},
+	[1011] = {
+		"http://www.roblox.com/asset/?id=105624907",
+		"M870",
+		"The Remington M870 is a 12-gauge tactical shotgun effective at short range."},
+	[1012] = {
+		"http://www.roblox.com/asset/?id=105624948",
+		"MP5",
+		"The MP5 is a tactical submachine gun effective at medium range. Uses a 9 x 19mm round with a 30-round magazine."},
+	[1015] = {
+		"http://www.roblox.com/asset/?id=105624877",
+		"Enfield",
+		"The Lee Enfield is a bolt-action rifle effective at long range. Uses a 7.7 x 56mm round with a 10-round magazine."},
+	[1016] = {
+		"http://www.roblox.com/asset/?id=110657188",
+		"M14",
+		"The M14 is a powerful automatic rifle effective at long range. Uses a 7.62 x 51mm round with a 20-round magazine."},
+	[1017] = {
+		"http://www.roblox.com/asset/?id=105624880",
+		"Fedorov",
+		"The Fedorov Avtomat is a powerful Soviet-era assault rifle effective at medium range. Uses a 6.5x50mm round with a 25-round magazine."},
+	[1018] = {
+		"http://www.roblox.com/asset/?id=105624964",
+		"SKS",
+		"The SKS is a Soviet-era semi-automatic rifle effective at long range. Uses a 7.62 x 39mm round with a 10-round magazine."},
+	[1019] = {
+		"http://www.roblox.com/asset/?id=105624955",
+		"PPSH",
+		"The PPSH is a Soviet-era submachine gun effective at short range. Uses a 7.62 x 25mm round with a 35-round magazine."},
+	[1021] = {
+		"http://www.roblox.com/asset/?id=583466574",
+		"Patriot",
+		"The 'Patriot' is a modified XM16E1 rifle with a short barrel and no rear stock. Uses 5.56 x 45mm ammunition."},
+	[1022] = {
+		"http://www.roblox.com/asset/?id=108375342",
+		"SCAR-L",
+		"The SCAR-L is a powerful assault rifle effective at long range. Compatible with 5.56 x 45mm STANAG magazines."},
+	[1023] = {
+		"http://www.roblox.com/asset/?id=108375339",
+		"G36K",
+		"The G36K is a powerful assault rifle effective at long range. Compatible with 5.56 x 45mm STANAG magazines."},
+	[1024] = {
+		"http://www.roblox.com/asset/?id=108400580",
+		"M249",
+		"The M249 is a powerful light machine gun effective at long range. Uses a 5.56 x 45mm M249 box magazine."},
+	[1025] = {
+		"http://www.roblox.com/asset/?id=108235165",
+		"Auto-5",
+		"The Auto-5 is a semi-automatic shotgun effective at short range. Uses 20-gauge pellets."},
+	[1026] = {
+		"http://www.roblox.com/asset/?id=108375353",
+		"PP-19",
+		"The PP-19 submachine gun is a powerful military weapon effective at long range. Uses a 9 x 18mm round with a 64-round magazine."},
+	[1027] = {
+		"http://www.roblox.com/asset/?id=108235184",
+		"M1014",
+		"The M1014 is a powerful military shotgun effective at short range. Uses 12-gauge pellets."},
+	[1028] = {
+		"http://www.roblox.com/asset/?id=108375358",
+		"M3",
+		"The M3 Grease Gun is a World War II-era submachine gun effective at medium range. Uses a 9 x 19mm round with a 30-round magazine."},
+	[1029] = {
+		"http://www.roblox.com/asset/?id=116674256",
+		"FAL",
+		"The FAL is a powerful battle rifle effective at long range. Uses a 7.62 x 51mm round."},
+	[1030] = {
+		"http://www.roblox.com/asset/?id=117005560",
+		"G3",
+		"The G3 is a powerful battle rifle effective at long range. Uses a 7.62 x 51mm round."},
+	[1031] = {
+		"http://www.roblox.com/asset/?id=116674269",
+		"Mk 17",
+		"The Mk 17 is a powerful and accurate battle rifle effective at long range. Uses a 7.62 x 51mm round."},
+	[1032] = {
+		"http://www.roblox.com/asset/?id=116674263",
+		"HK21",
+		"The HK21 is a powerful light machine gun effective at medium range. Uses a 7.62 x 51mm round."},
+	[1033] = {
+		"http://www.roblox.com/asset/?id=116674265",
+		"HK417",
+		"The HK417 is a powerful battle rifle effective at long range. Uses a 7.62 x 51mm round."},
+	[1034] = {
+		"http://www.roblox.com/asset/?id=116674277",
+		"RPK",
+		"The RPK is a powerful light machine gun effective at medium range. Uses a 7.62 x 39mm round."},
+	[1035] = {
+		"http://www.roblox.com/asset/?id=116674227",
+		"AK-12",
+		"The AK-12 is a modern assault rifle effective at medium range. Uses a 7.62 x 39mm round."},
+	[1036] = {
+		"http://www.roblox.com/asset/?id=116674273",
+		"OTs-14",
+		"The OTs-14 is a funny-looking, yet powerful submachine gun effective at medium range. Uses a 7.62 x 39mm round."},
+	[1037] = {
+		"http://www.roblox.com/asset/?id=116674241",
+		"AK-104",
+		"The AK-104 is a modern assault rifle effective at medium range. Uses a 7.62 x 39mm round."},
+	[1038] = {
+		"http://www.roblox.com/asset/?id=116674244",
+		"AKM",
+		"The AKM is a modernized variant of the AK-47 effective at medium range. Uses a 7.62 x 39mm round."},
+	[1039] = {
+		"http://www.roblox.com/asset/?id=116674235",
+		"AK-74",
+		"The AK-74 is an assault rifle effective at medium range. Uses a 5.45 x 39mm round."},
+	[1040] = {
+		"http://www.roblox.com/asset/?id=116674252",
+		"AN-94",
+		"The AN-94 is a powerful, modern assault rifle effective at medium range. Uses a 5.45 x 39mm round."},
+	[1041] = {
+		"http://www.roblox.com/asset/?id=116674248",
+		"AKS-74U",
+		"The AKS-74U is a submachine gun variant of the AK-74 Assault Rifle effective at medium range. Uses a 5.45 x 39mm round."},
+	[2002] = {
+		"http://www.roblox.com/asset/?id=105624909",
+		"M1911",
+		"This Colt M1911 is a high-powered pistol effective at medium range. Uses a .45 caliber round with a seven-round magazine."},
+	[2003] = {
+		"http://www.roblox.com/asset/?id=105624915",
+		"Makarov",
+		"The Makarov pistol is a lightweight pistol effective at close range. Uses a 9 x 18mm round with an eight-round magazine."},
+	[2004] = {
+		"http://www.roblox.com/asset/?id=105624897",
+		"M9",
+		"The Beretta M9 is a high-capacity handgun effective at medium range. Uses a 9 x 19mm round."},
+	[2005] = {
+		"http://www.roblox.com/asset/?id=105624969",
+		"TEC-9",
+		"This fully automatic Intratec TEC-9 is a powerful handgun effective at close range. Uses a 9 x 19mm round."},
+	[2006] = {
+		"http://www.roblox.com/asset/?id=105624958",
+		"Revolver",
+		"The Model 625 is a powerful revolver effective at medium range. Uses a .45 caliber round with a six-round capacity."},
+	[2019] = {
+		"http://www.roblox.com/asset/?id=718691860",
+		"Model 29",
+		"The Model 29 is a powerful revolver effective at medium range. Uses a .44 magnum round with a six-round capacity."},
+	[2007] = {
+		"http://www.roblox.com/asset/?id=105624971",
+		"USP45",
+		"The USP45 is a powerful handgun effective at medium range. Uses a .45 caliber round with a 12-round magazine."},
+	[2010] = {
+		"http://www.roblox.com/asset/?id=105624875",
+		"CZ75",
+		"The CZ 75 is a personal-defense handgun effective at medium range. Uses a 9 x 19mm round with a 12-round magazine."},
+	[2011] = {
+		"http://www.roblox.com/asset/?id=105624883",
+		"G18",
+		"This fully automatic Glock 18 is a powerful handgun effective at medium range. Uses a 9 x 19mm round."},
+	[2013] = {
+		"http://www.roblox.com/asset/?id=108235186",
+		"Model 459",
+		"The Model 459 is a pistol with a high capacity magazine. Uses a 9 x 19mm round with a 14-round magazine."},
+	[2014] = {
+		"http://www.roblox.com/asset/?id=108235167",
+		"CBJ-MS",
+		"The CBJ-MS is a powerful military handgun. Uses a 9 x 19mm round."},
+	[2015] = {
+		"http://www.roblox.com/asset/?id=116704751",
+		"Uzi",
+		"The Uzi is a powerful automatic military weapon effective at medium range. Uses a 9 x 19mm round."},
+	[2016] = {
+		"http://www.roblox.com/asset/?id=116704747",
+		"M93R",
+		"The M93R is a high-capacity automatic handgun effective at medium range. Uses a 9 x 19mm round."},
+	[2017] = {
+		"http://www.roblox.com/asset/?id=116704745",
+		"G37",
+		"The G37 is a powerful handgun effective at medium range. Uses a .45 caliber round with a 12-round magazine."},
+	[2018] = {
+		"http://www.roblox.com/asset/?id=116704753",
+		"Mk 23",
+		"The Mk 23 is a powerful handgun effective at medium range. Uses a .45 caliber round with a 12-round magazine."},
+    }
